2cb4d22f1ed61d721cb5ec04e64bbcdbb0f67b19
Kato <143938160+JTG-RX@users.noreply.github.com>
2025-11-29 15:45:03 -0500

    Simplify function to extract first letters from words
    Refactored function to return first letters of words.

--- take a list of strings and return a list containing only the first letter of each word.py
+++ take a list of strings and return a list containing only the first letter of each word.py
@@ -1,3 +1,4 @@
+#1
 def fristL(n):
     Litter = []
     for i in n:
@@ -5,3 +6,20 @@ def fristL(n):
             Litter.append(i)
         
     return Litter
+#2
+def fristL(n):
+    Litter = []
+    for i in n:
+       for l in i:
+           if l == l[0]:
+            Litter.append(i)
+        
+    return Litter
+
+#3
+def fristL(n):
+    Litter = []
+    for i in n:
+        Litter.append(i[0])
+    return Litter 
+    #just had to think simpler
