6666bdcf517bbb1733b0942256737071514152c5
Kato <143938160+JTG-RX@users.noreply.github.com>
2025-11-29 15:52:07 -0500

    Add function to filter strings from a list

--- returns all the numbers from a list that are greater than 5.py
+++ returns all the numbers from a list that are greater than 5.py
@@ -1,6 +1,14 @@
+#1
 def greater_than_5(n): 
   over5 = [] 
   for i in n:  
   if i >5: 
     over5.append(i) 
   return over5
+#2
+def only_strings(n):
+    strings =[]
+    for i in n:
+        if type(i) == str:
+            strings.append(i)
+    return strings
